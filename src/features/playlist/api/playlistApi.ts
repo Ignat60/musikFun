814a1a30218a7c67818bcb370c2291668f077da7
Ignat60 <dedaignat@gmail.com>
2025-10-20 22:07:37 +0300

    dev:creat store

--- src/features/playlist/api/playlistApi.ts
+++ src/features/playlist/api/playlistApi.ts
@@ -1,4 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type {
+  FetchPlaylistsArgs,
+  PlaylistsResponse,
+} from "./playlistApi.types";
 
 // https://musicfun.it-incubator.app/api/1.0/
 export const playlistsApi = createApi({
@@ -11,7 +15,7 @@ export const playlistsApi = createApi({
   }),
   endpoints: (build) => {
     return {
-      fetchPlaylists: build.query<string, string>({
+      fetchPlaylists: build.query<PlaylistsResponse, FetchPlaylistsArgs>({
         query: () => {
           return {
             method: "get",
